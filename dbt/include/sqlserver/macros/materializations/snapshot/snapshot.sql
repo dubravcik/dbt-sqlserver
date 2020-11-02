d759d1f78aeece4e2c41af592e1a1d1126be8f73
Michal Dubravcik <michal.dubravcik@gmail.com>
2020-11-02 18:43:19 +0100

    overwrite create_columns to fix add column error

--- dbt/include/sqlserver/macros/materializations/snapshot/snapshot.sql
+++ dbt/include/sqlserver/macros/materializations/snapshot/snapshot.sql
@@ -2,3 +2,12 @@
   -- Clean up the snapshot temp table
   {% do drop_relation(staging_relation) %}
 {% endmacro %}
+
+
+{% macro sqlserver__create_columns(relation, columns) %}
+  {% for column in columns %}
+    {% call statement() %}
+      alter table {{ relation }} add "{{ column.name }}" {{ column.data_type }};
+    {% endcall %}
+  {% endfor %}
+{% endmacro %}
